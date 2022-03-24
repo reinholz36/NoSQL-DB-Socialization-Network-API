73e960bcb10bed6c2ff76d5c67383b02f66979a0
reinholz36 <dreinholz28@gmail.com>
2022-03-24 18:19:12 -0400

    updated users model

---
+++ models/Users.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const UsersSchema = new Schema (
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true 
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Users'
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false 
+    }
+)
+
+//Friends total counts 
+UsersSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+// User model 
+const Users = model('Users', UsersSchema);
+
+module.exports = Users; 
