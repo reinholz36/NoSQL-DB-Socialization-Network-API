92980039a13334b14a14835f20d7ef328d06925c
reinholz36 <dreinholz28@gmail.com>
2022-03-25 19:06:19 -0400

    corrected server error

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 const Thoughts = require('./Thoughts');
-const Users = require('/Users');
+const Users = require('./Users');
 
 module.exports = { Users, Thoughts };
--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(express.static('public'));
 
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/NoSQL-Social-Network-API', {
-    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true 
 });
